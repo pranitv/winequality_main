b7637302a737218dd630b52b45f4bff1574c81b0
pranitv <pranitvithalkar7@gmail.com>
2021-03-21 16:57:53 +0530

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    discription="its a wine q packages",
+    author="pranit vithalkar",
+    packages=find_packages(),
+    license="MIT"
+)
