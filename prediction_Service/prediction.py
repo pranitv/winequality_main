67548cc99d96890e5998623c16f77ca357c2afb5
pranitv <pranitvithalkar7@gmail.com>
2021-03-23 17:06:06 +0530

    test cases updated

--- prediction_Service/prediction.py
+++ prediction_Service/prediction.py
@@ -54,7 +54,7 @@ def validate_input(dict_request):
     def _validate_values(col, val):
         schema = get_schema()
 
-        if not (schema[col]["min"] <= float(dict_request[col][0]) <= schema[col]["max"]) :
+        if not (schema[col]["min"] <= float(dict_request[col]) <= schema[col]["max"]) :
             raise NotInRange
 
     for col, val in dict_request.items():
@@ -68,7 +68,7 @@ def form_response(dict_request):
     if validate_input(dict_request):
         data = dict_request.values()
         # print(data)
-        data = np.array([[float(i[0]) for i in list(data)]])
+        data = np.array([[float(i) for i in list(data)]])
         response = predict(data)
         return response
 
